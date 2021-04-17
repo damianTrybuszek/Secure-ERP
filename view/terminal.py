a834ad71b5ed3c2a07620e8ebe51abb3f67a0063
Damian Trybuszek <damian.trybuszek@gmail.com>
2021-04-17 13:42:40 +0200

    Print table function implemented!

--- view/terminal.py
+++ view/terminal.py
@@ -31,43 +31,47 @@ def print_general_results(result, label):
 # |   1    | Sidewinder | missile  |
 # \--------------------------------/
 def print_table(table, headers):
-    counters = []
+
+    longest_words = []
+
     for i in range(len(headers)): 
-        counters.append(headers[i])
+        longest_words.append(headers[i])
     
     for element in table:
         for i in range(len(element)):
-            if len(element[i]) > len(str(counters[i])):
-                counters[i] = element[i]
-    sum_counters = len(headers)*4
-    
-    for element in counters:
-        sum_counters += len(str(element))
-    
+            if len(element[i]) > len(str(longest_words[i])):
+                longest_words[i] = element[i]
+
     cols_width = ["|"]
 
-    for element in counters:
-        cols_width.append((len(str(element))+6)*"-"+"|")
+    for element in longest_words:
+        cols_width.append(((len(str(element)))+4)*"-"+"|")
 
+    sum_counters = -2
+    
+    for element in cols_width:
+        sum_counters += len(str(element))
 
     rows = []
     row_second = ["|"]
 
     for i  in range(len(headers)):
-        row_second.append("  "+headers[i]+(len(str(counters[i]))-len(str(headers[i])))*" "+"|")
+        temp_width_headers = len(str(longest_words[i]))-len(str(headers[i]))
+        row_second.append("  "+headers[i]+(temp_width_headers*" ")+"  |")
     rows.append(row_second)
 
     for element in table:
         row_second = ["|"]
         for i in range(len(element)):
-            row_second.append("  "+element[i]+(len(str(counters[i]))-len(str(element[i])))*" "+"|")
+            temp_width_elements = len(str(longest_words[i]))-len(str(element[i]))
+            row_second.append("  "+element[i]+(temp_width_elements*" ")+"  |")
         rows.append(row_second)
         
-    
-
     print("/"+(sum_counters*"-")+"\\")
-    for data in rows:
-        print(''.join(data))
+    for i in range(len(rows)):
+        if i != 0:
+            print(''.join(cols_width))
+        print(''.join(rows[i]))
     print("\\"+(sum_counters*"-")+"/")
 
 
