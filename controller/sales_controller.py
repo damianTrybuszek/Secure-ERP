7798c624c9fb1fe28a60d6efd325e04fa7399658
unknown <dubickidymitr@gmail.com>
2021-04-18 17:38:50 +0200

    Count transactions between function added

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -7,6 +7,9 @@ CUSTOMER_INDEX = 1
 PRODUCT_INDEX = 2
 PRICE_INDEX = 3
 DATE_INDEX = 4
+# YEAR_INDEX = "0:4"
+# MONTH_INDEX = 5:7
+# DAY_INDEX = 8:10
 
 
 def list_transactions():
@@ -76,13 +79,44 @@ def get_biggest_revenue_product():
                 view.print_message("The biggest revenue product is " + f"'{lines[line][PRODUCT_INDEX]}'")
 
 
-
-    
-
-
-
 def count_transactions_between():
-    view.print_error_message("Not implemented yet.")
+    lines = file_handling.read_table_from_file(sales.DATAFILE, separator=';')
+    user_input_start_date = view.get_input(sales.HEADERS[DATE_INDEX])
+    user_input_end_date = view.get_input(sales.HEADERS[DATE_INDEX])
+    user_range_date_list = [user_input_start_date, user_input_end_date]
+    counter = 0
+    months = dict({"01": 1, "02": 2, "03": 3, "04": 4, "05": 5, "06": 6, "07": 7, "08": 8, "09": 9, "10": 10, "11": 11, "12": 12})
+    days = dict({"01": 1, "02": 2, "03": 3, "04": 4, "05": 5, "06": 6, "07": 7, "08": 8, "09": 9, "10": 10,
+                 "11": 11, "12": 12, "13": 13, "14": 14, "15": 15, "16": 16, "17": 17, "18": 18, "19": 19, "20": 20,
+                 "21": 21, "22": 22, "23": 23, "24": 24, "25": 25, "26": 26, "27": 27, "28": 28, "29": 29, "30": 30, "31": 31})
+
+    for line in range(len(lines)):
+        transaction_date = lines[line][DATE_INDEX]
+        month_of_user_input_start_date = months.get(user_range_date_list[0][5:7])
+        month_of_user_input_end_date = months.get(user_range_date_list[1][5:7])
+        month_of_transaction = months.get(transaction_date[5:7])
+        day_of_user_input_start_date = days.get(user_range_date_list[0][8:10])
+        day_of_user_input_end_date  = days.get(user_range_date_list[1][8:10])
+        day_of_transaction = days.get(transaction_date[8:10])
+
+        if user_range_date_list[0][0:4] < transaction_date[0:4] and transaction_date[0:4] < user_range_date_list[1][0:4]:
+            counter += 1
+        elif user_range_date_list[0][0:4] == transaction_date[0:4] or transaction_date[0:4] == user_range_date_list[1][0:4]:
+            if user_range_date_list[0][0:4] == transaction_date[0:4]:
+                if month_of_user_input_start_date <= month_of_transaction:
+                    if month_of_user_input_start_date < month_of_transaction:
+                        counter += 1
+                    else:
+                        if day_of_user_input_start_date <= day_of_transaction:
+                            counter += 1
+            if transaction_date[0:4] == user_range_date_list[1][0:4]:
+                if month_of_transaction <= month_of_user_input_end_date:
+                    if month_of_transaction < month_of_user_input_end_date:
+                        counter += 1
+                    else:
+                        if day_of_transaction <= day_of_user_input_end_date:
+                            counter += 1
+    view.print_message(f"The number of transactions made during the specified time period: {counter}")
 
 
 def sum_transactions_between():
