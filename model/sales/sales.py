4d38637ddc28e4461486dfd3dcc8abcbfd758249
unknown <dubickidymitr@gmail.com>
2021-04-22 19:54:11 +0200

    CRUD functions finished

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -26,7 +26,7 @@ def list_of_transactions():
     return TRANSACTIONS
 
 
-def new_transaction():
+def add_new_transaction():
     transaction_list = []
     for i in range(len(HEADERS)):
         user_input = view.get_input(HEADERS[i])
@@ -35,10 +35,45 @@ def new_transaction():
     return TRANSACTIONS
 
 
-def valid_input():
+def valid_input(list_of_transactions):
     user_input = view.get_input("Do you want to save changes? Yes or No?").upper()
-    if user_input == "YES" or user_input == "Y" or user_input == "NO" or user_input == "N": 
+    if user_input == "YES" or user_input == "Y":
+        data_manager.write_table_to_file(DATAFILE, list_of_transactions, separator=';')
+    elif user_input == "NO" or user_input == "N":
         return user_input
     else:
-        new_valid_input = valid_input()
+        new_valid_input = valid_input(list_of_transactions)
         return new_valid_input
+
+
+def update_list():
+    user_input_id = view.get_input(HEADERS[ID_INDEX])
+    counter = 0
+    for transaction in range(len(TRANSACTIONS)):  
+        if user_input_id == TRANSACTIONS[transaction][ID_INDEX]:
+            for i in range(len(HEADERS) - 1):
+                user_input_update = view.get_input(HEADERS[i + 1])
+                TRANSACTIONS[transaction][i + 1] = user_input_update
+                counter += 1
+        else:
+            counter = 0
+    return TRANSACTIONS, counter
+
+
+def delete_list_element():
+    user_input_id = view.get_input(HEADERS[ID_INDEX])
+    counter = 0
+    for transaction in range(len(TRANSACTIONS)):
+        if user_input_id == TRANSACTIONS[transaction][ID_INDEX]:
+            TRANSACTIONS.pop(transaction)
+            counter += 1
+        else:
+            counter = 0
+    return TRANSACTIONS, counter
+
+
+def update_and_delete_transaction(list_of_transactions, counter):
+    if counter == 0:
+        view.print_message("There is no such transaction with this Id")
+    else:
+        valid_input(list_of_transactions)
